d9a48dafd80d3f9b35487988f694fde34b2f1e12
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-08-26 20:09:57 -0400

    add server code to take in edit requests from the client to change the longURL of a specific shortURL and add edit button inside urls_index that redirects to a form inside urls_show that make the post request

--- express_server.js
+++ express_server.js
@@ -2,35 +2,25 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const generateRandomString = () => {
   const randomString = (Math.random() + 1).toString(36).substring(6);
   return randomString;
 };
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
-
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send('Hello!');
-});
-
 app.listen(PORT, () => {
-  console.log(`Example app listening port ${PORT}`);
+  console.log(`TinyApp listening port ${PORT}`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send('<html><body><b>Hello World!</b></body></html>');
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls", (req, res) => {
@@ -38,14 +28,22 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[`${shortURL}`] = [`${req.body.longURL}`];
+  res.redirect('/urls');
 });
+
 
 app.get("/u/:shorturl", (req, res) => {
   const shortURL = req.params.shorturl;
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
+});
+
+app.post("/urls/:shorturl", (req, res) => {
+  urlDatabase[req.params.shorturl] = req.body.newLongURL;
+  res.redirect("/urls");
 });
 
 app.get("/urls/:shorturl", (req, res) => {
@@ -54,13 +52,11 @@ app.get("/urls/:shorturl", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
-  urlDatabase[`${shortURL}`] = [`${req.body.longURL}`];
-  res.redirect(`/u/${shortURL}`);
-});
-
 app.post("/urls/:shorturl/delete", (req, res) => {
   delete urlDatabase[req.params.shorturl];
   res.redirect('/urls');
+});
+
+app.get("/", (req, res) => {
+  res.redirect("/urls");
 });
