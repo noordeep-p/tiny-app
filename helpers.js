3570024aecf77f091974f01bc746129c2e8a2be0
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-08-28 20:42:17 -0400

    create seperate file for helper functions and import them into server to DRY up code

---
+++ helpers.js
@@ -0,0 +1,16 @@
+const getUserUrlsById = (id, urlDatabase) => {
+  const urlsByUser = {};
+  for (const url in urlDatabase) {
+    if (urlDatabase[url].userId === id) {
+      urlsByUser[url] = urlDatabase[url];
+    }
+  }
+  return urlsByUser;
+};
+
+const generateRandomString = () => {
+  const randomString = (Math.random() + 1).toString(36).substring(6);
+  return randomString;
+};
+
+module.exports = getUserUrlsById, generateRandomString;
